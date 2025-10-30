9df82bb3a89ce7e673c154f7b860c4fda542beae
T3hx <a230385707@gmail.com>
2025-10-30 20:15:03 +0100

    change: update app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
-	selector: "app-root",
-	imports: [RouterOutlet],
-	templateUrl: "./app.html",
-	styleUrl: "./app.css",
+  selector: 'app-root',
+  imports: [RouterOutlet, Navbar],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
-	protected readonly title = signal("angular-project");
+  protected readonly title = signal('angular-project');
 }
