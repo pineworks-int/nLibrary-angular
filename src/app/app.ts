e395f8fb9d684bf7fef329d30109c56e38d6aed8
T3hx <a230385707@gmail.com>
2025-11-02 20:04:55 +0100

    feat: importation du composant home dans le composant app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
