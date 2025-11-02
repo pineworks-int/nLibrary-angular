459f54dfa7c8efca5441c3c3f7c1a2e21fac8e33
T3hx <a230385707@gmail.com>
2025-11-02 20:26:39 +0100

    Revert "Feat/home-body (#2)"
    This reverts commit 5c95106f0ae25c5e29cb400c551cb1f2ee08a5fb.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { Home } from './components/home/home';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Home],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('angular-project');
+	protected readonly title = signal("angular-project");
 }
